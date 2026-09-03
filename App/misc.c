57f025d003e0aabba2dec233584620d17deb5240
Dim0s <89480281+Dim0s@users.noreply.github.com>
2026-09-03 06:57:46 +0500

    Update misc.c

--- App/misc.c
+++ App/misc.c
@@ -48,14 +48,14 @@ const uint8_t     scan_delay_10ms                  =   210 / 10;   // 210ms
 #ifdef ENABLE_FEAT_F4HWN
     const uint16_t    dual_watch_count_after_tx_10ms   =  420;         // 4.2 sec after TX ends
     const uint16_t    dual_watch_count_after_rx_10ms   =  1000 / 10;   // 1 sec after RX ends ?
-    const uint16_t    dual_watch_count_after_1_10ms    =  100 / 10;   // 100ms
-    const uint16_t    dual_watch_count_after_2_10ms    =  20;         // 4.2 sec
+    const uint16_t    dual_watch_count_after_1_10ms    =  5000 / 10;   // 5 sec
+    const uint16_t    dual_watch_count_after_2_10ms    =  820;         // 4.2 sec
     const uint16_t    dual_watch_count_noaa_10ms       =    70 / 10;   // 70ms
 #else
     const uint16_t    dual_watch_count_after_tx_10ms   =  3600 / 10;   // 3.6 sec after TX ends
     const uint16_t    dual_watch_count_after_rx_10ms   =  1000 / 10;   // 1 sec after RX ends ?
-    const uint16_t    dual_watch_count_after_1_10ms    =  100 / 10;   // 100ms
-    const uint16_t    dual_watch_count_after_2_10ms    =  100 / 10;   // 100ms
+    const uint16_t    dual_watch_count_after_1_10ms    =  5000 / 10;   // 5 sec
+    const uint16_t    dual_watch_count_after_2_10ms    =  3600 / 10;   // 3.6 sec
     const uint16_t    dual_watch_count_noaa_10ms       =    70 / 10;   // 70ms
 #endif
 
