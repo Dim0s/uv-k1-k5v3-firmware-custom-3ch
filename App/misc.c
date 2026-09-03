449881f1ec671f991f4986ed2dadaf1a1e442acc
Dim0s <89480281+Dim0s@users.noreply.github.com>
2026-09-03 05:52:17 +0500

    Update misc.c

--- App/misc.c
+++ App/misc.c
@@ -48,7 +48,7 @@ const uint8_t     scan_delay_10ms                  =   210 / 10;   // 210ms
 #ifdef ENABLE_FEAT_F4HWN
     const uint16_t    dual_watch_count_after_tx_10ms   =  420;         // 4.2 sec after TX ends
     const uint16_t    dual_watch_count_after_rx_10ms   =  1000 / 10;   // 1 sec after RX ends ?
-    const uint16_t    dual_watch_count_after_1_10ms    =  5000 / 10;   // 5 sec
+    const uint16_t    dual_watch_count_after_1_10ms    =  100 / 10;   // 100ms
     const uint16_t    dual_watch_count_after_2_10ms    =  420;         // 4.2 sec
     const uint16_t    dual_watch_count_noaa_10ms       =    70 / 10;   // 70ms
 #else
